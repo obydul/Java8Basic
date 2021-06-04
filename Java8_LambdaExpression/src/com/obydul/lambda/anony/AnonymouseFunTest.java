d4ab3370d7e507584b804f562844705c541c60fb
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-06-04 09:13:41 +0600

    java 8 lambda expression anonymous function

---
+++ Java8_LambdaExpression/src/com/obydul/lambda/anony/AnonymouseFunTest.java
@@ -0,0 +1,16 @@
+package com.obydul.lambda.anony;
+
+public class AnonymouseFunTest {
+	
+	//public void an1() {
+	//	System.out.println("Tradition process");
+	//}
+	
+//	(){
+//		System.out.println("Lambda function");
+//	}
+//
+//	public static void main(String[] args) {
+//		//new AnonymouseFunTest().an1();
+//	}
+}
