53c61eb2bbafe33d4a5e7550a79bb0a784b3edf3
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-06-05 09:08:49 +0600

    Lambda Expression

---
+++ Java8_LambdaExpression/src/com/obydul/lambda/store/BookDAO.java
@@ -0,0 +1,17 @@
+package com.obydul.lambda.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDAO {
+
+	public List<Book> getBooks() {
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(101, "Core Java", 400));
+		books.add(new Book(201, "Hibernate", 456));
+		books.add(new Book(301, "Spring", 350));
+		books.add(new Book(401, "WebService", 540));
+		return books;
+	}
+
+}
