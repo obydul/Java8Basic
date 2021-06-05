37cb1855793ae02bbaf86f7e316df8a2cd205bd0
Md Obydul Islam <mdobydulislam100@gmail.com>
2021-06-05 09:18:34 +0600

    Lambda Expression

--- Java8_LambdaExpression/src/com/obydul/lambda/store/Book.java
+++ Java8_LambdaExpression/src/com/obydul/lambda/store/Book.java
@@ -5,6 +5,10 @@ public class Book {
 	private String name;
 	private int pages;
 	
+	public Book() {
+		
+	}
+	
 	public Book(int id, String name, int pages) {
 		this.id = id;
 		this.name = name;
@@ -28,6 +32,42 @@ public class Book {
 	public void setPages(int pages) {
 		this.pages = pages;
 	}
+	@Override
+	public String toString() {
+		return "id=" + id + ", name=" + name + ", pages=" + pages + "\n";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + pages;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (pages != other.pages)
+			return false;
+		return true;
+	}
+	
+	
+	
 	
 	
 
---
+++ Java8_LambdaExpression/src/com/obydul/lambda/store/BookService.java
@@ -0,0 +1,26 @@
+package com.obydul.lambda.store;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class BookService {
+	public List<Book> getBookSort() {
+		List<Book> books = new BookDAO().getBooks();
+		Collections.sort(books, new MyComparator());
+		return books;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new BookService().getBookSort());
+	}
+}
+
+class MyComparator implements Comparator<Book> {
+
+	@Override
+	public int compare(Book o1, Book o2) {
+		return o1.getName().compareTo(o2.getName());
+	}
+
+}
